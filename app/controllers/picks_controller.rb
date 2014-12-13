975148889b5491b624f123aa8a93fa8c3786cd35
Patrick Kelly <pk6281@mcla.edu>
2014-12-13 15:45:09 -0500

    Added add pick functionality

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -6,8 +6,15 @@ class PicksController < ApplicationController
   def new
   	@pick = Pick.new
   	@user = current_user
-  	@round = 1
-    @pickNumber = 1
+  	@round = last_round
+    @pickNumber = last_pick
+    if(@pickNumber == 5)
+        @round +=1
+        @pickNumber=1
+    else
+        @pickNumber+=1
+    end
+    @team = next_team(@pickNumber, @round)
   end
 
   def create
@@ -22,11 +29,31 @@ class PicksController < ApplicationController
 
   def index
   	@picks = Pick.all
+    @round = last_round
+    @pickNumber = last_pick
   end
 
   private
   	def pick_params
   		params.require(:pick).permit(:round, :pick, :user, :team, :player)
   	end
+    def last_round
+      last_pick = Pick.order("created_at").last
+      last_round = last_pick.round
+    end
+    def last_pick
+        last_pick = Pick.order("created_at").last
+        l_pick = last_pick.pick
+    end
+    def next_team (pickNum,roundNum)
+      oddTeam = ["Team 1","Team 2","Team 3","Team 4","Team 5"]
+      evenTeam = oddTeam.reverse
+      pickNum -=1
+      if(roundNum%2 == 0)
+        return evenTeam[pickNum]
+      else
+        return oddTeam[pickNum]
+      end
+    end
 
 end
